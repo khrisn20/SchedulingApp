ff3ce595eb1fda1f971a747fbca4686bef5ae4ab
khrisn20 <khrisrian.narvaez@yahoo.com>
2022-07-12 05:26:17 -0700

    Practice Commit and Push

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import DAO.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+        stage.setTitle("First Screen");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+
+        JDBC.openConnection();
+
+        JDBC.closeConnection();
+
+        launch(args);
+    }
+}
