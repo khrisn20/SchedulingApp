d959b757b6a1f0de66c0048d6aff528cf0ecb133
khrisn20 <khrisrian.narvaez@yahoo.com>
2022-07-19 04:02:08 -0700

    Main Menu Screen Navigation

--- src/controller/MainMenu.java
+++ src/controller/MainMenu.java
@@ -2,25 +2,49 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenu {
+
+    Stage stage;
+    Parent scene;
+
     @FXML
-    void OnActionDisplayAddAppointment(ActionEvent event) {
+    void OnActionDisplayAddAppointment(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/CreateAppointment.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    @FXML
+    void OnActionDisplayAddCustomer(ActionEvent event) throws IOException {
+            stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("/view/CreateCustomer.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+    }
+
+    @FXML
+    void OnActionDisplayUpdateAppointment(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/UpdateAppointment.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
 
     }
 
     @FXML
-    void OnActionDisplayAddCustomer(ActionEvent event) {
-
-    }
-
-    @FXML
-    void OnActionDisplayUpdateAppointment(ActionEvent event) {
-
-    }
-
-    @FXML
-    void OnActionDisplayUpdateCustomer(ActionEvent event) {
-
+    void OnActionDisplayUpdateCustomer(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/UpdateCustomer.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 }
