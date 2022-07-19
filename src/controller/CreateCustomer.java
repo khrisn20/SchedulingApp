185d982aabe2a827cbc3b84dd8f1ca251fe66410
khrisn20 <khrisrian.narvaez@yahoo.com>
2022-07-19 04:13:22 -0700

    Create Customer Screen Navigation

--- src/controller/CreateCustomer.java
+++ src/controller/CreateCustomer.java
@@ -2,15 +2,31 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateCustomer {
-    @FXML
-    void OnActionDisplayMainMenu(ActionEvent event) {
+    Stage stage;
+    Parent scene;
 
+    @FXML
+    void OnActionDisplayMainMenu(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    void OnActionSaveCustomer(ActionEvent event) {
-
+    void OnActionSaveCustomer(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 }
