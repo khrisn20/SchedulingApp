6c4c861f422a7bcfe4e7fbe84092332fafa81f8f
khrisn20 <khrisrian.narvaez@yahoo.com>
2022-07-19 05:13:35 -0700

    Created Appointment Class

---
+++ src/model/Appointment.java
@@ -0,0 +1,141 @@
+package model;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private int appointment_id;
+    private String title;
+    private String description;
+    private String location;
+    private String type;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private LocalDateTime created_date;
+    private String created_by;
+    private Timestamp last_updated;
+    private int customer_id;
+    private int user_id;
+    private int contact_id;
+
+    public Appointment(int appointment_id, String title, String description, String location, String type, LocalDateTime start, LocalDateTime end, LocalDateTime created_date, String created_by, Timestamp last_updated, int customer_id, int user_id, int contact_id) {
+        this.appointment_id = appointment_id;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.type = type;
+        this.start = start;
+        this.end = end;
+        this.created_date = created_date;
+        this.created_by = created_by;
+        this.last_updated = last_updated;
+        this.customer_id = customer_id;
+        this.user_id = user_id;
+        this.contact_id = contact_id;
+    }
+
+    public int getAppointment_id() {
+        return appointment_id;
+    }
+
+    public void setAppointment_id(int appointment_id) {
+        this.appointment_id = appointment_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    public LocalDateTime getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(LocalDateTime created_date) {
+        this.created_date = created_date;
+    }
+
+    public String getCreated_by() {
+        return created_by;
+    }
+
+    public void setCreated_by(String created_by) {
+        this.created_by = created_by;
+    }
+
+    public Timestamp getLast_updated() {
+        return last_updated;
+    }
+
+    public void setLast_updated(Timestamp last_updated) {
+        this.last_updated = last_updated;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getContact_id() {
+        return contact_id;
+    }
+
+    public void setContact_id(int contact_id) {
+        this.contact_id = contact_id;
+    }
+}
+
