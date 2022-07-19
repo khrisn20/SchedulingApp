24760eeba9e473b1a1a6b66c437da435b19ea1da
khrisn20 <khrisrian.narvaez@yahoo.com>
2022-07-19 05:55:05 -0700

    Created Contact Class

---
+++ src/model/Contact.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Contact {
+    private int contact_id;
+    private String contact_name;
+    private String email;
+
+    public int getContact_id() {
+        return contact_id;
+    }
+
+    public void setContact_id(int contact_id) {
+        this.contact_id = contact_id;
+    }
+
+    public String getContact_name() {
+        return contact_name;
+    }
+
+    public void setContact_name(String contact_name) {
+        this.contact_name = contact_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Contact(int contact_id, String contact_name, String email) {
+        this.contact_id = contact_id;
+        this.contact_name = contact_name;
+        this.email = email;
+    }
+}
