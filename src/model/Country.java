ecd4a4a6b097cbb0df8e354161b003d61b2b2f7a
khrisn20 <khrisrian.narvaez@yahoo.com>
2022-07-19 06:02:50 -0700

    Created Country Class

---
+++ src/model/Country.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class Country {
+    private int country_id;
+    private String country;
+    private LocalDateTime create_date;
+    private String created_by;
+    private Timestamp last_update;
+    private String last_updated_by;
+
+    public Country(int country_id, String country, LocalDateTime create_date, String created_by, Timestamp last_update, String last_updated_by) {
+        this.country_id = country_id;
+        this.country = country;
+        this.create_date = create_date;
+        this.created_by = created_by;
+        this.last_update = last_update;
+        this.last_updated_by = last_updated_by;
+    }
+
+    public int getCountry_id() {
+        return country_id;
+    }
+
+    public void setCountry_id(int country_id) {
+        this.country_id = country_id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public LocalDateTime getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(LocalDateTime create_date) {
+        this.create_date = create_date;
+    }
+
+    public String getCreated_by() {
+        return created_by;
+    }
+
+    public void setCreated_by(String created_by) {
+        this.created_by = created_by;
+    }
+
+    public Timestamp getLast_update() {
+        return last_update;
+    }
+
+    public void setLast_update(Timestamp last_update) {
+        this.last_update = last_update;
+    }
+
+    public String getLast_updated_by() {
+        return last_updated_by;
+    }
+
+    public void setLast_updated_by(String last_updated_by) {
+        this.last_updated_by = last_updated_by;
+    }
+}
